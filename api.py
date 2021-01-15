d853b70abcd4c87391eaa2b4aa895bff03b63ed4
berli0z <40333748+berli0z@users.noreply.github.com>
2021-01-15 18:12:57 +0100

    limiting to 40 results per category

--- api.py
+++ api.py
@@ -12,6 +12,7 @@ from config import DOC_URL, WAIT_SECONDS, user, pwd
 
 def update():
     df = pd.read_csv(DOC_URL)
+    df = df.groupby('category').tail(40)
     df.to_csv("output.csv", index=False)
     print("data updated!")
     threading.Timer(WAIT_SECONDS, update).start()
@@ -39,6 +40,7 @@ class Memes(Resource):
     def get(self):
         data = pd.read_csv('output.csv')  # read CSV
         data.index.name = 'index'
+        data = data.groupby('category').tail(40)
         data = data.to_json(orient='index')  # convert dataframe to dictionary
         dat = json.loads(data)
         return {'data': dat}, 200  # return data and 200 OK code
